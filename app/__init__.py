75dd39b9930594e89386f7be65ff0496c268c031
gigtarget <jatingots9@gmail.com>
2025-05-31 16:34:20 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from app.db import init_db
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "your_secret_key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///needle_logs.db"
+    import os app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     init_db(app)
