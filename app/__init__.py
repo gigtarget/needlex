c37563dac9e4cb75797aed68ebf9c22dc7aa3e51
gigtarget <jatingots9@gmail.com>
2025-05-31 16:07:50 -0400

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from app.routes import main
+from app.db import init_db
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "your_secret_key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///needle_logs.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    init_db(app)
+    app.register_blueprint(main)
+
+    return app
