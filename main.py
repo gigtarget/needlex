b39b06fc7dbdcce6a49f83b91ccd4dceb382699e
gigtarget <jatingots9@gmail.com>
2025-06-01 21:05:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# main.py
+
 from app import create_app
 import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # Railway uses PORT env variable
     app.run(host="0.0.0.0", port=port, debug=True)
